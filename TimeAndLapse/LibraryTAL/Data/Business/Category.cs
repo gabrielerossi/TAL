ba8d8de202ab19b4a84572a061e71b1f573ace4a
Gabriele Rossi <gabriele.rossi.97@live.it>
2015-12-29 14:15:56 +0100

    Importate le tabelle e create classi partial

---
+++ TimeAndLapse/LibraryTAL/Data/Business/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTAL.Data.Designer
+{
+    public partial class Category
+    {
+        partial void InitializePartial()
+        {
+            CategoryId = new Guid();
+        }
+    }
+}
